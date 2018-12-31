73b3ab457d99583612216c35783032b1e1fc1f11
Rishik01 <Rishikchand05@gmail.com>
2018-12-31 09:15:29 -0800

    Update whois.js

--- commands/whois.js
+++ commands/whois.js
@@ -2,10 +2,6 @@ const Discord = require("discord.js")
 
 module.exports.run = async (message, bot, args) => {
     let whodat = message.mentions.users.first()
-    if(!whodat){
-         let whodat = message.author
-    } 
-    console.log(whodat);
 }
 
 module.exports.help = {
